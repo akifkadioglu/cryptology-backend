492fea0d21b9683516058671711fa2c68c89a9ef
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-23 23:06:14 +0300

    edited route

--- routes/init.go
+++ routes/init.go
@@ -15,7 +15,7 @@ func Init() {
 	Web()
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "9000" // Default port if not specified
+		port = ":9000" // Default port if not specified
 	}
 	E.Logger.Fatal(E.Start(port))
 }
