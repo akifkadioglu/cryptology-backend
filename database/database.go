7ed4064f5f36dead292ff15dd53aa7df53bcb6e2
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-23 20:50:43 +0300

    added db

--- database/database.go
+++ database/database.go
@@ -3,22 +3,21 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"setup/env"
 	models "setup/models"
 )
 
 var db *gorm.DB
 var err error
-var dbAddress = env.GoDotEnvVariable("DB_USERNAME") + ":" +
+/* var dbAddress = env.GoDotEnvVariable("DB_USERNAME") + ":" +
 	env.GoDotEnvVariable("DB_PASSWORD") +
 	"@tcp(" +
 	env.GoDotEnvVariable("DB_HOST") +
 	":" +
 	env.GoDotEnvVariable("DB_PORT") +
 	")/"
-
+ */
 func Init() gorm.DB {
-	dns := dbAddress + env.GoDotEnvVariable("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dns := " mysql://bf455b2579977c:43489f3a@us-cdbr-east-06.cleardb.net/heroku_b5def9f32733d0c?reconnect=true"
 	db, err = gorm.Open(mysql.Open(dns))
 	db.AutoMigrate(&models.User{}, &models.Record{})
 	if err != nil {
