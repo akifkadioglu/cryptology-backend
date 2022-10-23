c0f6c188435674f3c2d48886509cb193f8118329
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-24 00:49:43 +0300

    fixed GetRecord func

--- controllers/RecordsController/GetRecord.go
+++ controllers/RecordsController/GetRecord.go
@@ -3,7 +3,9 @@ package recordscontroller
 import (
 	"net/http"
 	"setup/database"
+	"setup/helpers"
 	"setup/models"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +13,7 @@ import (
 func GetRecord(c echo.Context) error {
 	var record models.Record
 	db := database.DBManager()
-	db.Order("created_at desc").First(&record)
+	db.Where("`user_id` = " + strconv.Itoa(helpers.User(c).ID)).Order("created_at desc").First(&record)
 	return c.JSON(http.StatusOK, map[string]models.Record{
 		"record": record,
 	})
