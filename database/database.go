41b17c8317dbabcde20fd4f5290022e890551df3
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-23 23:26:13 +0300

    edited db

--- database/database.go
+++ database/database.go
@@ -17,7 +17,7 @@ var err error
 	")/"
  */
 func Init() gorm.DB {
-	dns := "admin:root12345@tcp(aws-simplified.c8i70zggrjmm.us-east-1.rds.amazonaws.com:3306)/cryptology_homework"
+	dns := "admin:root12345@tcp(aws-simplified.c8i70zggrjmm.us-east-1.rds.amazonaws.com:3306)/cryptology_homework?parseTime=true"
 	db, err = gorm.Open(mysql.Open(dns))
 	db.AutoMigrate(&models.User{}, &models.Record{})
 	if err != nil {
